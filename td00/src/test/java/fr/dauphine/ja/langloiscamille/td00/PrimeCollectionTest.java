215f13ea3cbfc6410f187cb774f9de5a922ca4ff
camillelanglois3 <camille.langlois@dauphine.eu>
2020-09-17 16:53:34 +0200

    Adding PrimeCollectionTest

---
+++ td00/src/test/java/fr/dauphine/ja/langloiscamille/td00/PrimeCollectionTest.java
@@ -0,0 +1,20 @@
+package fr.dauphine.ja.langloiscamille.td00;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class PrimeCollectionTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testPrimeCollection()
+    {
+        assertTrue( true );
+    }
+}
